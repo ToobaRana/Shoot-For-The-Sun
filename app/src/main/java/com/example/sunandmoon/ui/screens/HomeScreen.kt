66a7425ba06ad4638f273589da297f92057f6ee2
toobarana <tooba.iar@gmail.com>
2023-03-28 23:45:18 +0200

    Basic layout for homescreen with suncards (with hardcode)

--- app/src/main/java/com/example/sunandmoon/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/sunandmoon/ui/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.sunandmoon.ui.screens
 
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,54 +9,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.sunandmoon.ui.components.SunCard
 import com.example.sunandmoon.viewModel.SunViewModel
+import com.example.sunandmoon.R
+
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(sunViewModel: SunViewModel = viewModel()){
 
-
     val sunUiState by sunViewModel.sunUiState.collectAsState()
+
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
-
-
-
         },
-
 
         content = {innerPadding ->
             LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
-                    .padding(top = 20.dp)
             )
 
             {
 
+                item {
+                    SunCard("Sunrise", painterResource(id = R.drawable.sunrise), "05:31")
+                    SunCard("Solar noon", painterResource(id = R.drawable.solarnoon), "13:43")
+                    SunCard("Sunset", painterResource(id = R.drawable.sunset), "19:51")
 
-                /*items(alpacaPartyList){alpacaParty ->
-
-                    val totalVotes = alpacaViewModel.totalVotes()
-
-                    AlpacaCard(
-                        alpacaParty,
-                        checkDistrict(chosenDistrict, votesUiState, alpacaParty), totalVotes, votesUiState)
-                }*/
-
+                }
             }
 
-
         },
-        bottomBar = {
 
-
-        }
+        bottomBar = {}
     )
-
 }
+
+
+
+
+
+
