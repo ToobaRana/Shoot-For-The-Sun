a43621aa9b4b2d64c2f9aba256e1e6a2f4c6f269
Medina <medinm@uio.no>
2023-04-27 18:35:30 +0200

    values fixed for table screen

--- app/src/main/java/com/example/sunandmoon/ui/screens/TableScreen.kt
+++ app/src/main/java/com/example/sunandmoon/ui/screens/TableScreen.kt
@@ -115,29 +115,34 @@ fun TableView(modifier: Modifier,tableViewModel: TableViewModel = viewModel(), t
                         items(apiDateTableList) { date ->
                             //println(date)
 
-                            println(apiDateTableList.size)
+
+                            println(i)
                             var elementInTableUiStateList = date.split("T")
 
                             var sunriseTime = elementInTableUiStateList[1]
                             var day = elementInTableUiStateList[0]
                             var monthInt = day.split("-")[1].toInt()
-                            println(monthInt)
+                            //println(monthInt)
 
-                            println("Our CalculationsTime: " + tableUIState.calculationsDateTableList[monthInt-1])
+                            //println("Our CalculationsTime: " + tableUIState.calculationsDateTableList[i-1])
+                            println("monthInt: " + monthInt)
 
-                            TableCard(
-                                apiSunTime = sunriseTime,
-                                day = day,
-                                calculationSunTime = tableUIState.calculationsDateTableList[monthInt-1],
-                                modifier = modifier
-                                    .background(if (date.indexOf(day) % 2 == 0) Color.White else Color.LightGray)
-                                    .padding(8.dp)
-                            )
 
-                            i+=1
+
+                                    TableCard(
+                                        apiSunTime = sunriseTime,
+                                        day = day,
+                                        calculationSunTime = tableUIState.calculationsDateTableList[monthInt-1]  ,
+                                        modifier = modifier
+                                            .background(if (date.indexOf(day) % 2 == 0) Color.White else Color.LightGray)
+                                            .padding(8.dp)
+                                    )
+
+
 
 
                         }
+
 
                     }
                     Spacer(modifier = modifier.height(200.dp))
--- app/src/main/java/com/example/sunandmoon/viewModel/TableViewModel.kt
+++ app/src/main/java/com/example/sunandmoon/viewModel/TableViewModel.kt
@@ -51,36 +51,55 @@ class TableViewModel : ViewModel() {
             var calculationsDateTableList = mutableListOf<String>()
 
             val sameDaysList = getSameDaysInYear(tableUiState.value.chosenDate)
+            val sameDaysListFromJanuary = getSameDaysInYearFromJanuary(tableUiState.value.chosenDate)
+            println(sameDaysList)
+            println(sameDaysListFromJanuary)
 
-
-            for (date in sameDaysList.sorted()){
-                println("This is the date" + date)
+            for (date in sameDaysListFromJanuary.sorted()){
                 var stringDate = date.toString() + " 00:00"
                 var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
                 var dateTime = LocalDateTime.parse(stringDate, formatter)
 
-
                 var calculationSunTime = getSunRiseNoonFall(dateTime, tableUiState.value.timeZoneOffset, tableUiState.value.latitude, tableUiState.value.longitude)
                 println(calculationSunTime)
                 println(dateTime)
+                if (tableUiState.value.chosenSunType == "Sunrise"){
+                    calculationsDateTableList.add(calculationSunTime[0])
+
+                }
+
+                if (tableUiState.value.chosenSunType == "SolarNoon"){
+                    calculationsDateTableList.add(calculationSunTime[1])
+                }
+
+                if (tableUiState.value.chosenSunType == "Sunset"){
+                    calculationsDateTableList.add(calculationSunTime[2])
+                }
+
+            }
+
+            for (date in sameDaysList.sorted()){
+
+                println(date)
+                println("This is the date" + date)
 
                 if (tableUiState.value.chosenSunType == "Sunrise"){
                     sunRiseTime = dataSource.fetchSunrise3Data("sun", 59.933333, 10.716667, date.toString(), "+01:00" ).properties.sunrise.time
                     apiDateTableList.add(sunRiseTime)
-                    calculationsDateTableList.add(calculationSunTime[0])
+
 
                 }
 
                 if (tableUiState.value.chosenSunType == "SolarNoon"){
                     solarNoon = dataSource.fetchSunrise3Data("sun", 59.933333, 10.716667, date.toString(), "+01:00" ).properties.solarnoon.time
                     apiDateTableList.add(solarNoon)
-                    calculationsDateTableList.add(calculationSunTime[1])
+
                 }
 
                 if (tableUiState.value.chosenSunType == "Sunset"){
                     sunSetTime = dataSource.fetchSunrise3Data("sun", 59.933333, 10.716667, date.toString(), "+01:00" ).properties.sunset.time
                     apiDateTableList.add(sunSetTime)
-                    calculationsDateTableList.add(calculationSunTime[2])
+
                 }
             }
 
@@ -120,6 +139,29 @@ class TableViewModel : ViewModel() {
                 sameDays.add(sameDayOfMonth)
 
             }
+
+        }
+        //println(sameDays.sorted())
+
+        return sameDays.sorted()
+    }
+    fun getSameDaysInYearFromJanuary(date: LocalDate): List<LocalDate> {
+
+        val daysInMonth = listOf(31, 28, 31, 30, 31, 30, 31, 31 ,30, 31, 30, 31)
+
+        val year = date.year
+        val sameDays = mutableListOf<LocalDate>()
+
+        for (month in 1..12) {
+            val daysInThisMonth = daysInMonth[month-1]
+            val dayToUse = min(daysInThisMonth, date.dayOfMonth)
+
+
+
+                val sameDayOfMonth = LocalDate.of(year, month, dayToUse)
+                sameDays.add(sameDayOfMonth)
+
+
 
         }
         //println(sameDays.sorted())
