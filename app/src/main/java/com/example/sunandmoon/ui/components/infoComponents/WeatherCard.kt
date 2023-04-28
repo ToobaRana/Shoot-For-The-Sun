1de1327e4c6fe3bd5b32fc5f0a476c18d96098cf
toobarana <tooba.iar@gmail.com>
2023-04-28 13:38:54 +0200

    Base for info-screen

--- app/src/main/java/com/example/sunandmoon/ui/components/infoComponents/WeatherCard.kt
+++ app/src/main/java/com/example/sunandmoon/ui/components/infoComponents/WeatherCard.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.sp
 import com.example.sunandmoon.R
 import com.example.sunandmoon.ui.components.SunPositionComponent
 
+
 @Composable
 fun WeatherCard(modifier: Modifier){
 
