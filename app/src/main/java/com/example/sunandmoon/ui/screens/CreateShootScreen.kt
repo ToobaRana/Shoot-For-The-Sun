6457edf3c5ae604615b3358c2fb2d09893208c9f
Adskan <adrian.skansen@hotmail.com>
2023-05-05 12:53:54 +0200

    begynte på createshoot

--- app/src/main/java/com/example/sunandmoon/ui/screens/CreateShootScreen.kt
+++ app/src/main/java/com/example/sunandmoon/ui/screens/CreateShootScreen.kt
@@ -1,25 +1,31 @@
 package com.example.sunandmoon.ui.screens
 
 import android.location.Location
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+
+
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sunandmoon.R
 import com.example.sunandmoon.ui.components.CalendarComponent
 import com.example.sunandmoon.ui.components.NavigationComposable
+import com.example.sunandmoon.viewModel.CreateShootViewModel
 import com.example.sunandmoon.viewModel.ShootInfoViewModel
 import java.time.LocalDateTime
 
@@ -27,27 +33,79 @@ import java.time.LocalDateTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CreateShootScreen(modifier: Modifier, navigateToNext: () -> Unit, shootInfoViewModel: ShootInfoViewModel = viewModel()){
+fun CreateShootScreen(modifier: Modifier, navigateToNext: () -> Unit, createShootViewModel: CreateShootViewModel = viewModel()){
 
-    val shootInfoUIState by shootInfoViewModel.shootInfoUIState.collectAsState()
+    val createShootUIState by createShootViewModel.createShootUIState.collectAsState()
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
-
         content = {innerPadding ->
+            val focusManager = LocalFocusManager.current
+
             LazyColumn(
                 modifier = modifier
                     .fillMaxSize()
                     .padding(innerPadding)
-                    .padding(top = 20.dp)
+                    .padding(top = 8.dp)
             )
 
             {
+                item{
+                    Row(horizontalArrangement = Arrangement.Center) {
+
+                        Spacer(modifier = modifier.fillMaxWidth(0.05f))
+
+                        TextField(value = createShootUIState.name,
+                            label = { Text("Title") },
+                            onValueChange = {name->
+                                createShootViewModel.updateShootName(name);
+                            },
+                            placeholder = { Text("My Shoot") },
+                            modifier = modifier.fillMaxWidth(0.9f),
+                            leadingIcon = {
+                                Icon(
+                                    painterResource(R.drawable.pencil),
+                                    "Edit text pencil icon",
+                                    Modifier,
+                                    MaterialTheme.colorScheme.primary
+                                )
+                            },
+                            colors = TextFieldDefaults.textFieldColors(
+                                //cursorColor = MaterialTheme.colorScheme.primary,
+                                textColor = MaterialTheme.colorScheme.onSurface,
+                                containerColor = MaterialTheme.colorScheme.background,
+                                unfocusedIndicatorColor = MaterialTheme.colorScheme.onSurface,
+                                unfocusedLabelColor = MaterialTheme.colorScheme.onSurface
+                            ),
+                            keyboardOptions = KeyboardOptions(
+                                imeAction = ImeAction.Done
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onDone = {
+                                    focusManager.clearFocus()
+                                }
+                            )
+
+
+                        )
+                        Spacer(modifier = modifier.fillMaxWidth(0.05f))
+                    }
+
+
+
+                }
+                item{
+                    Spacer(modifier = modifier.size(20.dp))
+                    Divider(modifier = modifier.fillMaxWidth().height(3.dp), color = MaterialTheme.colorScheme.onSurface)
+                    Spacer(modifier = modifier.size(20.dp))
+                }
                 item {
                     LocationSearch(modifier = modifier)
                     CalendarComponent(modifier)
 
+
                     Button(onClick = {
+                        //save stuff
                         navigateToNext()
                     }) {
                         Text(text = "Save")
