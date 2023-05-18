ce0c3a675b23e174683b1cc308c7978701e018af
Adskan <adrian.skansen@hotmail.com>
2023-05-18 22:09:21 +0200

    Cards now work with clock

--- app/src/main/java/com/example/sunandmoon/ui/components/userInputComponents/SunPositionTime.kt
+++ app/src/main/java/com/example/sunandmoon/ui/components/userInputComponents/SunPositionTime.kt
@@ -1,30 +1,38 @@
 package com.example.sunandmoon.ui.components.userInputComponents
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.sunandmoon.R
-import com.example.sunandmoon.getSunRiseNoonFall
 import com.example.sunandmoon.ui.components.buttonComponents.CreateShootSunPositionCard
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.LocalTime
-import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Local
 
 @Composable
-fun SunPositionTime(modifier: Modifier, updateTime: (time: LocalTime) -> Unit, sunTimes: List<String>) {
+fun SunPositionTime(
+    modifier: Modifier,
+    updateTime: (time: LocalTime) -> Unit,
+    sunTimes: List<String>
+) {
     Card(
         modifier.fillMaxWidth(0.95f),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)
     ) {
-        Row(modifier = modifier.fillMaxWidth().padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(15.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
 
             val color = MaterialTheme.colorScheme.tertiary
 
@@ -47,10 +55,10 @@ fun SunPositionTime(modifier: Modifier, updateTime: (time: LocalTime) -> Unit, s
                 ),
                 containerColor = color,
 
-            )
+                )
             CreateShootSunPositionCard(
                 modifier = modifier,
-                updateTime = {  updateTime(LocalTime.parse(sunTimes[1])) },
+                updateTime = { updateTime(LocalTime.parse(sunTimes[1])) },
                 painter = painterResource(
                     R.drawable.solarnoon
                 ),
