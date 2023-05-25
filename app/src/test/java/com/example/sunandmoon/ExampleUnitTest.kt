20cf3f8ffccd5c3f3a21aed092c5567692a90ee0
Henrik Brunvatne Olafsen <henrbol@uio.no>
2023-05-25 18:16:50 +0200

    Broken unit test

--- app/src/test/java/com/example/sunandmoon/ExampleUnitTest.kt
+++ app/src/test/java/com/example/sunandmoon/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.sunandmoon
 
+import android.location.Location
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,9 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+        Location("").apply {
+            latitude = 10.0
+            longitude = 10.0
+        }
     }
 }
