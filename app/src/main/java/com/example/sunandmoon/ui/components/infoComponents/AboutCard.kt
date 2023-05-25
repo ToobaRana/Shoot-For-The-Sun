2c87ed85461a07e54302e3fe08dd191202e9289e
Adskan <adrian.skansen@hotmail.com>
2023-05-25 14:22:01 +0200

    added disclaimer

--- app/src/main/java/com/example/sunandmoon/ui/components/infoComponents/AboutCard.kt
+++ app/src/main/java/com/example/sunandmoon/ui/components/infoComponents/AboutCard.kt
@@ -1,14 +1,16 @@
 package com.example.sunandmoon.ui.components.infoComponents
 
-import android.text.method.LinkMovementMethod
-import android.widget.TextView
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.*
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -56,6 +58,8 @@ fun AboutCard(modifier: Modifier) {
                 linkTextColor = MaterialTheme.colorScheme.background,
                 fontSize = 18.sp)
             }
+            item{Text(text = "${stringResource(id = R.string.Disclaimer)}\n${ stringResource(R.string.Disclaimertext)}")}
+
 
         }
 
